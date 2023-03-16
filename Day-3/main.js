708c8e19f0bf7fa5427df5d15095256dc99683b9
Rahul <91734971+r-vb@users.noreply.github.com>
2023-03-16 22:18:16 +0530

    Add files via upload

---
+++ Day-3/main.js
@@ -0,0 +1,26 @@
+/* var, let and const are the three types of datatype */
+
+// var marks = 90
+// if( marks < 70 && marks >= 40 )
+// {   console.log("you belong to A div");
+// }else{
+//     console.log("you belong to B div");
+// }
+
+
+// var x = 2;
+// function cube(x){
+//     console.log(x*x*x);
+// }
+// cube(x);
+
+// var x = document.getElementById("pra");
+// var y = document.getElementById("inp");
+// console.log(x);
+// console.log(y);
+
+var y = document.getElementById("submit");
+y.addEventListener("click",function()
+{
+    alert("rahul");
+})
